b7c142bbfa8c2fe6e7f0f4ab2249bf096a55b2de
jainshrk <shreyans.jain@db.com>
2025-09-24 19:50:43 +0530

    REACTivity

--- waltz-ng/client/playpen/DummyComponent.jsx
+++ waltz-ng/client/playpen/DummyComponent.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React from  "react"
+import {useState} from "react";
 
 const DummyComponent = (props) => {
-	return (
-		<div style={{ background: '#f0f0f0', borderRadius: '4px', textAlign: 'center' }}>
-			Hello, From react!
-		</div>
+    const [value, setValue] = useState(0);
+    const onClick = () => setValue(prev => prev + 1);
+
+    return (
+        <div>
+            <div style={{ background: '#f0f0f0', borderRadius: '4px', textAlign: 'center' }}>
+                <p>Hello from React!</p>
+                <button className="btn btn-default"
+                        onClick={onClick}>
+                    Increment
+                </button>
+                <p>{value}</p>
+            </div>
+        </div>
 	);
 };
 
